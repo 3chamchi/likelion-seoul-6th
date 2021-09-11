d99f7e136fe0943e1b26ee33d0f8233d25bbf2ed
chamchi <44095761+3chamchi@users.noreply.github.com>
2021-09-12 02:28:50 +0900

    기본 model, admin 코드 작성

--- week5/likelion-web/posts/admin.py
+++ week5/likelion-web/posts/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin  # 장고 어드민을 사용하기 위한 임포트
 
-# Register your models here.
+from .models import Post  # Post 모델 사용을 위한 임포트, 점(.)은 현재 파일의 위치를 나타냄
+
+
+@admin.register(Post)  # 장고 어드민에 Post 모델 등록, @함수명() -> 데코레이터 함수
+class PostAdmin(admin.ModelAdmin):  # 장고 어드민 ModelAdmin 클래스 상속
+    list_display = ('title', 'content', 'created_at',)  # 목록 페이지에서 보여줄 속성 지정
--- week5/likelion-web/posts/models.py
+++ week5/likelion-web/posts/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.db import models  # 장고 모델을 사용하기 위한 임포트
 
-# Create your models here.
+
+class Post(models.Model):  # 장고 모델 Post 작성
+    title = models.CharField(verbose_name='제목', max_length=80,)  # Post 모델의 title 속성 정의
+    content = models.TextField(verbose_name='내용',)  # Post 모델의 content 속성 정의
+    created_at = models.DateTimeField(
+        verbose_name='생성일', blank=True, null=True, help_text='데이터가 생성된 일자입니다.',)  # Post 모델의 created_at 속성 정의
