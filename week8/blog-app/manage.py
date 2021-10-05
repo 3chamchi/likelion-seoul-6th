8df359a2cfe9b16082314cff6d00536181b35b59
dongwon <ceo@idealkr.com>
2021-10-05 16:44:14 +0900

    manage.py 충돌 해결

--- week8/blog-app/manage.py
+++ week8/blog-app/manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'confg.settings')
-=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
->>>>>>> main
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
