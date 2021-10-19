75a1623561308b9a20191e51d0bed10028efa0b0
chamchi <3chamchi@gmail.com>
2021-10-19 19:00:38 +0900

    week9 upgrade

--- week9/blog-app/config/urls.py
+++ week9/blog-app/config/urls.py
@@ -6,15 +6,10 @@ from django.urls import path, include
 from posts.views import list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('', list, name='home'),  # 메인 페이지 지정
-
-    path('accounts/', include('accounts.urls')),  # accounts 앱의 urls 사용
-    path('posts/', include('posts.urls')),  # posts 앱의 urls 사용
-    path('scraps/', include('scraps.urls')),
-    path('photolog/', include('photolog.urls')),
+    path("admin/", admin.site.urls),
+    path("", list, name="home"),  # 메인 페이지 지정
+    path("accounts/", include("accounts.urls")),  # accounts 앱의 urls 사용
+    path("posts/", include("posts.urls")),  # posts 앱의 urls 사용
+    path("scraps/", include("scraps.urls")),
+    path("photolog/", include("photolog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
